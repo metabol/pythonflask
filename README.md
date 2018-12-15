73c0ea47479224d4f86afb15c4c99b75b7753b9b
ADM account for Nmaju <adm_nmajuoke1@emea.adsint.biz>
2018-12-15 03:53:46 +0100

    modify README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pythonflask
 Simple flask app
+
+docker build -t pythonflask:latest .
+
+docker run -d --name pythonflask -p 5000:5000 pythonflask:latest
+
+
+This conatiner will serve Traffic on localhost:5000
+
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized v1.0'
+    return 'Flask Dockerized'
 
+
+#Serve traffic on localhost:5000
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
